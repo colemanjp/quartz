68c9b91f4ac3c584c3f7e568b8641c8d3e1eab31
John Coleman <john.coleman@yale.edu>
2025-01-15 09:43:36 -0800

    Quartz sync: Jan 15, 2025, 9:43 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "colemanjp",
+    pageTitle: "blacksquarelabs",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quartz.blacksquarelabs.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
